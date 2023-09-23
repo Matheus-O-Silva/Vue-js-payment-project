4adfae674268b63cc38a8c65871a2e6b91040cfb
Matheus Oliveira <matheus.oliveira110101@gmail.com>
2023-09-23 10:21:19 -0300

    Adding vuex and preloader

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    loading: false,
   },
   getters: {
   },
   mutations: {
+    setLoading(state, isLoading) {
+      state.loading = isLoading;
+    },
   },
   actions: {
   },
